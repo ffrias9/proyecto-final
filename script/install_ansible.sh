7b6266c2a088f085e4898d25698863c9d74b37d7
ffrias9 <framuxs@hotmail.com>
2023-06-04 22:38:18 +0000

    Improvements

--- script/install_ansible.sh
+++ script/install_ansible.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-sudo yum remove python3 -y
+sudo yum remove python3 python36 ansible -y
 
-sudo yum install python3.9 -y
+python3 -m pip uninstall ansible -y
+
+sudo yum install python39 -y
 
 python3 -m pip install --user ansible
