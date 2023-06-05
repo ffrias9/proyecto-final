21b83099638e86a00ad83ba7481613b21fa8a875
ffrias9 <framuxs@hotmail.com>
2023-06-05 18:33:24 +0000

    Incluida instalación de molecule en el script

--- script/install_ansible_and_molecule.sh
+++ script/install_ansible_and_molecule.sh
@@ -4,6 +4,10 @@ sudo yum remove python3 python36 ansible -y
 
 python3 -m pip uninstall ansible -y
 
-sudo yum install python39 -y
+sudo yum install python39 python39-pip -y
 
 python3 -m pip install --user ansible
+
+python3 -m pip install --user molecule[docker]
+
+pip3 install --upgrade selinux
