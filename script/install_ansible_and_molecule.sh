106a57e4eb62e1946717b5b6d56e28ca9729c238
ffrias9 <framuxs@hotmail.com>
2023-06-05 20:08:42 +0000

    Fix errors

--- script/install_ansible_and_molecule.sh
+++ script/install_ansible_and_molecule.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo yum remove python3 python36 ansible -y
+#sudo yum remove python3 python36 ansible -y
 
-python3 -m pip uninstall ansible -y
+#python3 -m pip uninstall ansible -y
 
 sudo yum install python39 python39-pip -y
 
