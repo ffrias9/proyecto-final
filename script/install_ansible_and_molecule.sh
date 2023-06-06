9b68062ab9bf136adb98a1464ca87dc3ede3608a
ffrias9 <framuxs@hotmail.com>
2023-06-06 08:34:41 +0000

    Fix error

--- script/install_ansible_and_molecule.sh
+++ script/install_ansible_and_molecule.sh
@@ -10,28 +10,32 @@ read -p "Do you want to continue? (y/n): " yesornot
 
 if [[ "$yesornot" == "y" ]]; then
 	echo "Remove Ansible if is it install..."
-	python3 -m pip uninstall ansible -y > /dev/null
+#	python3 -m pip uninstall ansible -y > /dev/null
 
 	echo "Removing old versions of Python..."
-	sudo yum remove python3 python36 ansible -y > /dev/null
+#	sudo yum remove python3 python36 ansible -y > /dev/null
 
 	echo "Installing Python3.9 and pip..."
-	sudo yum install python39 python39-pip -y > /dev/null
+#	sudo yum install python39 python39-pip -y > /dev/null
 
 	echo "Installing Ansible..."
-	python3 -m pip install ansible --user > /dev/null
+#	python3 -m pip install ansible --user > /dev/null
 
 	echo "Installing Molecule-3.4..."
-	pip install molecule==3.4.0 > /dev/null
+	pip3 install molecule==3.4.0 > /dev/null
 
 	echo "Installing Molecule[docker]..."
-	pip install molecule[docker]==3.4.0 > /dev/null
+	pip3 install molecule[docker]==3.4.0 > /dev/null
 
 	echo "Installing Ansible-lint-5.1.1..."
-	pip install ansible-lint==5.1.1 > /dev/null
+	pip3 install ansible-lint==5.1.1 > /dev/null
 
 	echo "Deactivating SELinux..."
 	sudo setenforce 0 > /dev/null
+
+	echo "Upgrading SELinux for Python..."
+	sudo pip3 install --upgrade selinux
+
 elif [[ "$yesornot" == "n" ]]; then
 	echo "Ok! Bye!"
 	exit 0
