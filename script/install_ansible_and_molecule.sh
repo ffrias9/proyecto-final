b9802fbc9a9d6c342543b0ae1b9be005df481609
ffrias9 <framuxs@hotmail.com>
2023-06-06 06:47:42 +0000

    New version of script

--- script/install_ansible_and_molecule.sh
+++ script/install_ansible_and_molecule.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-#sudo yum remove python3 python36 ansible -y
+echo "Remove Ansible if is it install..."
+python3 -m pip uninstall ansible -y > /dev/null
 
-#python3 -m pip uninstall ansible -y
+echo "Removing old versions of Python..."
+sudo yum remove python3 python36 ansible -y > /dev/null
 
-sudo yum install python39 python39-pip -y
+echo "Installing Python3.9 and pip..."
+sudo yum install python39 python39-pip -y > /dev/null
 
-python3 -m pip install --user ansible
+echo "Installing Ansible..."
+python3 -m pip install ansible --user > /dev/null
 
-python3 -m pip install --user molecule[docker]
+echo "Installing Molecule-3.4..."
+pip install molecule==3.4.0 > /dev/null
 
-pip3 install --upgrade selinux
+echo "Installing Molecule[docker]..."
+pip install molecule[docker]==3.4.0 > /dev/null
+
+echo "Installing Ansible-lint-5.1.1..."
+pip install ansible-lint==5.1.1 > /dev/null
+
+echo "Deactivating SELinux..."
+sudo setenforce 0 > /dev/null
