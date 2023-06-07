67b70f2ad37c2fcd414285f2fb2b76ec42d375fe
ffrias <framuxs@hotmail.com>
2023-06-07 12:08:59 +0200

    Cambios y mejoras en Vagrantfile

---
+++ maquinas/slaves/debian.sh
@@ -0,0 +1,10 @@
+sudo apt-get -y update && sudo apt-get -y upgrade
+
+sudo apt-get -y install vim
+
+sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config;
+sudo systemctl restart sshd;
+
+echo "root:toor" | sudo chpasswd
+echo "PS1='\e[1;32m\u@\h:\e[m \e[1;36m\w\$ \e[m'" | sudo tee -a /etc/bashrc
+source .bashrc
