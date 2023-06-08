6a8f42a39e2e1c520f3de728afb83b3d15b73f99
ffrias9 <framuxs@hotmail.com>
2023-06-08 10:38:55 +0000

    Agregado fichero sql de inserción masiva de usuarios en la BD de Nextcloud

---
+++ script/nextcloud_users.sql
@@ -0,0 +1,10 @@
+/*THE PASSWORD IS ENCRYPTED. THE PASSWORD IS = "contraseña" FOR ALL USERS*/
+INSERT INTO oc_users (uid, displayname, password, uid_lower) VALUES
+('juan.garcia', 'Juan García', '2|$argon2i$v=19$m=16,t=2,p=1$eXR4RWQxQnN6Y0VyYXFDdg$7i/FKlzLRq/i2C/NLDGDZQ', 'juan.garcia'),
+('ana.rodriguez', 'Ana Rodríguez', '2|$argon2i$v=19$m=16,t=2,p=1$eXR4RWQxQnN6Y0VyYXFDdg$7i/FKlzLRq/i2C/NLDGDZQ', 'ana.rodriguez'),
+('luis.lopez', 'Luis Lopez', '2|$argon2i$v=19$m=16,t=2,p=1$eXR4RWQxQnN6Y0VyYXFDdg$7i/FKlzLRq/i2C/NLDGDZQ', 'luis.lopez'),
+('maria.martinez', 'María Martínez', '2|$argon2i$v=19$m=16,t=2,p=1$eXR4RWQxQnN6Y0VyYXFDdg$7i/FKlzLRq/i2C/NLDGDZQ', 'maria.martinez'),
+('jose.fernandez', 'José Fernandez', '2|$argon2i$v=19$m=16,t=2,p=1$eXR4RWQxQnN6Y0VyYXFDdg$7i/FKlzLRq/i2C/NLDGDZQ', 'jose.fernandez'),
+('laura.sanchez', 'Laura Sánchez', '2|$argon2i$v=19$m=16,t=2,p=1$eXR4RWQxQnN6Y0VyYXFDdg$7i/FKlzLRq/i2C/NLDGDZQ', 'laura.sanchez'),
+('carlos.garcia', 'Carlos García', '2|$argon2i$v=19$m=16,t=2,p=1$eXR4RWQxQnN6Y0VyYXFDdg$7i/FKlzLRq/i2C/NLDGDZQ', 'carlos.garcia'),
+('patricia.morales', 'Patricia Morales', '2|$argon2i$v=19$m=16,t=2,p=1$eXR4RWQxQnN6Y0VyYXFDdg$7i/FKlzLRq/i2C/NLDGDZQ', 'patricia.morales');
